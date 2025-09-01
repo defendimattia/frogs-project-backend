f5d48b0843012e5fb35d45c178c8060b0cdd7d1d
defendimattia <defendi.mattia92@gmail.com>
2025-09-01 15:18:09 +0200

    initial repo

---
+++ src/main/java/org/project/frogs/frogs_project_backoffice/FrogsProjectBackofficeApplication.java
@@ -0,0 +1,13 @@
+package org.project.frogs.frogs_project_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogsProjectBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrogsProjectBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/project/frogs/frogs_project_backoffice/FrogsProjectBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.frogs.frogs_project_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogsProjectBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
