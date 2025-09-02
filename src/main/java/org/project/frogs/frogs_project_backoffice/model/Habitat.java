3b162b1a34dd3122dfc0f704628d5b7734bcc7f0
defendimattia <defendi.mattia92@gmail.com>
2025-09-02 10:19:43 +0200

    Add habitat entity

---
+++ src/main/java/org/project/frogs/frogs_project_backoffice/model/Habitat.java
@@ -0,0 +1,84 @@
+package org.project.frogs.frogs_project_backoffice.model;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "habitats")
+public class Habitat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "habitat name cannot be empty")
+    @Size(min = 3, max = 50, message = "name must be min 3 and max 50 characters long")
+    private String name;
+
+    @Lob
+    private String description;
+
+    @ManyToMany(mappedBy = "habitats")
+    @JsonBackReference
+    private Set<Frog> frogs;
+
+    public Habitat() {
+    }
+
+    public Habitat(Integer id, String name, String description, Set<Frog> frogs) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.frogs = frogs;
+    }
+
+    public Habitat(Integer id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Frog> getFrogs() {
+        return this.frogs;
+    }
+
+    public void setFrogs(Set<Frog> frogs) {
+        this.frogs = frogs;
+    }
+
+}
