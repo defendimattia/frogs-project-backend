00ce06646f77282f47b82a7439493edb8fe814e5
defendimattia <defendi.mattia92@gmail.com>
2025-09-03 12:09:24 +0200

    Add search functionality in FrogController and corresponding method in FrogRepository

--- src/main/java/org/project/frogs/frogs_project_backoffice/controller/FrogController.java
+++ src/main/java/org/project/frogs/frogs_project_backoffice/controller/FrogController.java
@@ -1,11 +1,14 @@
 package org.project.frogs.frogs_project_backoffice.controller;
 
+import java.util.List;
+
 import org.project.frogs.frogs_project_backoffice.model.Frog;
 import org.project.frogs.frogs_project_backoffice.repository.FrogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -16,10 +19,20 @@ public class FrogController {
     @Autowired
     FrogRepository frogsRepository;
 
-    @GetMapping("")
+    @GetMapping
     public String index(Model model) {
 
         model.addAttribute("frogs", frogsRepository.findAll());
+
+        return "frogs/index";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam(name = "query") String query, Model model) {
+
+        List<Frog> filteredFrogs = frogsRepository.findByCommonNameIgnoreCaseContainingOrScientificNameIgnoreCaseContaining(query, query);
+
+        model.addAttribute("frogs", filteredFrogs);
 
         return "frogs/index";
     }
--- src/main/java/org/project/frogs/frogs_project_backoffice/repository/FrogRepository.java
+++ src/main/java/org/project/frogs/frogs_project_backoffice/repository/FrogRepository.java
@@ -1,8 +1,11 @@
 package org.project.frogs.frogs_project_backoffice.repository;
 
+import java.util.List;
+
 import org.project.frogs.frogs_project_backoffice.model.Frog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FrogRepository extends JpaRepository<Frog, Integer> {
 
+    public List<Frog> findByCommonNameIgnoreCaseContainingOrScientificNameIgnoreCaseContaining(String commonName, String scientificName);
 }
