a353292918d1204a4056b17ef8c8c6f24a7ff487
defendimattia <defendi.mattia92@gmail.com>
2025-09-08 16:17:57 +0200

    Refactor Frog model and templates for improved validation and display of conservation status and habitats

--- src/main/java/org/project/frogs/frogs_project_backoffice/model/Frog.java
+++ src/main/java/org/project/frogs/frogs_project_backoffice/model/Frog.java
@@ -15,7 +15,6 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
@@ -55,11 +54,11 @@ public class Frog {
 
     @ManyToOne
     @JoinColumn(name = "conservationStatus_id", nullable = false)
+    @NotNull(message = "select a conservation status")
     private ConservationStatus conservationStatus;
 
     @ManyToMany
     @JoinTable(name = "frog_habitat", joinColumns = @JoinColumn(name = "frog_id"), inverseJoinColumns = @JoinColumn(name = "habitat_id"))
-    @NotEmpty(message = "a frog must have at least one habitat")
     private Set<Habitat> habitats;
 
     public Frog() {
