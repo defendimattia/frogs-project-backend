95f8660d3d14fbf673e74ae6aaa877cfe43c0a17
defendimattia <defendi.mattia92@gmail.com>
2025-09-09 11:27:00 +0200

    Add HabitatRestController

---
+++ src/main/java/org/project/frogs/frogs_project_backoffice/controller/HabitatRestController.java
@@ -0,0 +1,66 @@
+package org.project.frogs.frogs_project_backoffice.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.project.frogs.frogs_project_backoffice.model.Habitat;
+import org.project.frogs.frogs_project_backoffice.service.HabitatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/habitats")
+public class HabitatRestController {
+
+    @Autowired
+    private HabitatService habitatService;
+
+    @GetMapping
+    public List<Habitat> index() {
+        List<Habitat> habitats = habitatService.getAllHabitats();
+        return habitats;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Habitat> show(@PathVariable Integer id) {
+
+        Optional<Habitat> habitatTry = habitatService.findById(id);
+
+        if (habitatTry.isEmpty()) {
+            return new ResponseEntity<Habitat>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Habitat>(habitatTry.get(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Habitat> store(@Valid @RequestBody Habitat habitat) {
+        return new ResponseEntity<Habitat>(habitatService.saveHabitat(habitat), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Habitat> update(@Valid @RequestBody Habitat habitat, @PathVariable Integer id) {
+
+        if (habitatService.findById(id).isEmpty()) {
+            return new ResponseEntity<Habitat>(HttpStatus.NOT_FOUND);
+        }
+
+        habitat.setId(id);
+        return new ResponseEntity<Habitat>(habitatService.saveHabitat(habitat), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Habitat> delete(@PathVariable Integer id) {
+
+        if (habitatService.findById(id).isEmpty()) {
+            return new ResponseEntity<Habitat>(HttpStatus.NOT_FOUND);
+        }
+
+        habitatService.deleteHabitat(id);
+        return new ResponseEntity<Habitat>(HttpStatus.NO_CONTENT);
+    }
+}
